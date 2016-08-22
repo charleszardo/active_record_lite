dc57b0cfde31e3f2503b2c8ae441729dc5d1ce97
Dave Charnuska <davecharnuska@gmail.com>
2016-08-22 13:59:27 -0400

    add #has_many and #belongs_to

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -1,7 +1,6 @@
 require_relative 'searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -36,18 +35,44 @@ end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
+    name = name.to_s
 
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :primary_key => :id,
+      :class_name => name.to_s.singularize.camelcase
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      key_val = self.send(options.foreign_key)
+
+      options
+        .model_class
+        .where(options.primary_key => key_val)
+        .first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do
+      key_val = self.send(options.primary_key)
+
+      options
+        .model_class
+        .where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
@@ -56,5 +81,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
