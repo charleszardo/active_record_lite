ac04f9064e3447102a6b4d2eaba58d5eb8b2368a
Dave Charnuska <davecharnuska@gmail.com>
2016-08-24 16:26:06 -0400

    begin Associatable#has_one_through

--- lib/associatable2.rb
+++ lib/associatable2.rb
@@ -1,9 +1,21 @@
 require_relative 'associatable'
 
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
-
   def has_one_through(name, through_name, source_name)
-    # ...
+    define_method(name) do
+
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_pk = through_options.primary_key
+      through_fk = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_pk = source_options.primary_key
+      source_fk = source_options.foreign_key
+
+      
+    end
   end
 end
